c7f703c96baa9c832edb84abfc10318fed46fa99
Aritra Banerjee <aritra.ban11@gmail.com>
2022-02-10 22:31:05 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Fb from './components/Fb';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='App-title'>Facebook Auth</h1>
+      <p className='App-intro'>
+        To get started, authenticate with Facebook
+      </p>
+      <Fb />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +13,33 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// import React from "react";
+// import { render } from "react-dom";
+// import Hello from "./Hello";
+// import FacebookLogin from "react-facebook-login";
+
+// const styles = {
+//   fontFamily: "sans-serif",
+//   textAlign: "center"
+// };
+
+// const responseFacebook = response => {
+//   console.log(response);
+// };
+
+// const App = () => (
+//   <div style={styles}>
+//     <Hello name="CodeSandbox" />
+//     <h2>Start editing to see some magic happen {"\u2728"}</h2>
+//     <FacebookLogin
+//       appId="1088597931155576"
+//       autoLoad={true}
+//       fields="name,email,picture"
+//       //onClick={componentClicked}
+//       callback={responseFacebook}
+//     />,
+//   </div>
+// );
+
+// render(<App />, document.getElementById("root"));
